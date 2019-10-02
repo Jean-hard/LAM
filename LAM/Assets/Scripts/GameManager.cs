8bd5a0fe5cbf7ff7ed2e57ca409ce5745cbbce43
paul thibault <paulthb1@gmail.com>
2019-10-02 10:41:43 +0200

    clean gameManager

--- LAM/Assets/Scripts/GameManager.cs
+++ LAM/Assets/Scripts/GameManager.cs
@@ -8,22 +8,14 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private PlayerManager player;
     [SerializeField]
-    private GameObject hallway;
-    [SerializeField]
-    private GameObject firstChamber;
-    [SerializeField]
     private GameObject firstMask;
 
-    private string clickedBtnName;
     private Vector3 currentMaskPosition;
 
     private Vector3 currentDestination;
     [SerializeField]
     private PlaneScript currentPlan;
     private PlaneScript nextPlan;
-
-    private Vector3 positionOnRightDoor = new Vector3(6f, 2.5f, 0);
-    private Vector3 positionOnLeftDoor = new Vector3(-6f, 2.5f, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -34,11 +26,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //si le personnage atteint la position de la porte de droite
-        //if (player.transform.position == positionOnRightDoor)
-        //{
-        //    ChangeScene();
-        //}
         if (player.transform.position == currentMaskPosition)
         {
             //placer le mask dans l'inventaire
@@ -51,17 +38,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //on gère un seul personnage donc autant gérer ça dans le gameManager
-    //public void MoveToRightDoor()
-    //{
-    //    player.targetPosition = positionOnRightDoor;
-    //}
-
-    //public void MoveToLeftDoor()
-    //{
-    //    player.targetPosition = positionOnLeftDoor;
-    //}
-
     public void MoveToDoor(DoorScript targetDoor)
     {
         currentDestination = targetDoor.doorPosition;
@@ -73,19 +49,6 @@ public class GameManager : MonoBehaviour
     public void ChangeScene()
     {
         player.targetPosition = player.PLAYER_BASE_POS;
-
-        //if(hallway.activeSelf)
-        //{
-        //    hallway.SetActive(false);
-        //    firstChamber.SetActive(true);
-        //    firstMask.SetActive(true);
-        //}
-        //else
-        //{
-        //    hallway.SetActive(true);
-        //    firstChamber.SetActive(false);
-        //    firstMask.SetActive(false);
-        //}
 
         Debug.Log("on change scene");
 
