c4d5ba06d25d8a10c79edabb308a5bc1f78a8612
VictorHamcha <victor.hamcha@edu.devinci.fr>
2019-10-02 12:58:13 +0200

    puzzle continue

--- LAM/Assets/Scripts/ScriptVictorHamcha/DragandDrop.cs
+++ LAM/Assets/Scripts/ScriptVictorHamcha/DragandDrop.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class DragandDrop : MonoBehaviour
 {
     private bool selected;
+    public List<GameObject> pos = new List<GameObject>();
     void Update()
     {
         if (selected==true)
         {
             Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector2(cursorPos.x, cursorPos.y);
+            gameObject.layer = 5;
         }
 
         if(Input.GetKeyUp(KeyCode.Mouse0))
         {
             selected = false;
+            gameObject.layer = 0;
         }
         
+
     }
 
     private void OnMouseOver()
@@ -25,6 +29,9 @@ public class DragandDrop : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             selected = true;
+
         }
     }
+
+    
 }
