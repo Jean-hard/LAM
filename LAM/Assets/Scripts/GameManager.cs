1275633c67f22f897c7151387bc8139599fcf3a7
paul thibault <paulthb1@gmail.com>
2019-10-02 10:03:56 +0200

    finished change plane and updated archi
    + delete astarpathfinding package

--- LAM/Assets/Scripts/GameManager.cs
+++ LAM/Assets/Scripts/GameManager.cs
@@ -17,39 +17,56 @@ public class GameManager : MonoBehaviour
     private string clickedBtnName;
     private Vector3 currentMaskPosition;
 
+    private Vector3 currentDestination;
+    [SerializeField]
+    private PlaneScript currentPlan;
+    private PlaneScript nextPlan;
+
     private Vector3 positionOnRightDoor = new Vector3(6f, 2.5f, 0);
     private Vector3 positionOnLeftDoor = new Vector3(-6f, 2.5f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentDestination = new Vector3(0f, 0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         //si le personnage atteint la position de la porte de droite
-        if (player.transform.position == positionOnRightDoor)
-        {
-            ChangeScene();
-        }
+        //if (player.transform.position == positionOnRightDoor)
+        //{
+        //    ChangeScene();
+        //}
         if (player.transform.position == currentMaskPosition)
         {
             //placer le mask dans l'inventaire
             firstMask.SetActive(false); //pour l'instant on le désactive juste
         }
+
+        if (player.transform.position == currentDestination)
+        {
+            ChangeScene();
+        }
     }
 
     //on gère un seul personnage donc autant gérer ça dans le gameManager
-    public void MoveToRightDoor()
-    {
-        player.targetPosition = positionOnRightDoor;
-    }
+    //public void MoveToRightDoor()
+    //{
+    //    player.targetPosition = positionOnRightDoor;
+    //}
 
-    public void MoveToLeftDoor()
+    //public void MoveToLeftDoor()
+    //{
+    //    player.targetPosition = positionOnLeftDoor;
+    //}
+
+    public void MoveToDoor(DoorScript targetDoor)
     {
-        player.targetPosition = positionOnLeftDoor;
+        currentDestination = targetDoor.doorPosition;
+        player.targetPosition = targetDoor.doorPosition;
+        nextPlan = targetDoor.planeNextDoor;
     }
 
     //a chaque changement de plan on placera le personnage à une position de base et on préparera un fade dans une coroutine
@@ -57,18 +74,24 @@ public class GameManager : MonoBehaviour
     {
         player.targetPosition = player.PLAYER_BASE_POS;
 
-        if(hallway.activeSelf)
-        {
-            hallway.SetActive(false);
-            firstChamber.SetActive(true);
-            firstMask.SetActive(true);
-        }
-        else
-        {
-            hallway.SetActive(true);
-            firstChamber.SetActive(false);
-            firstMask.SetActive(false);
-        }
+        //if(hallway.activeSelf)
+        //{
+        //    hallway.SetActive(false);
+        //    firstChamber.SetActive(true);
+        //    firstMask.SetActive(true);
+        //}
+        //else
+        //{
+        //    hallway.SetActive(true);
+        //    firstChamber.SetActive(false);
+        //    firstMask.SetActive(false);
+        //}
+
+        Debug.Log("on change scene");
+
+        nextPlan.OnActive();
+        currentPlan.OnDesactive();
+        currentPlan = nextPlan;
     }
 
     //ce délai servira à limiter les interactions juste après un changement de plan (et à éviter des bugs... surtout)
---
+++ LAM/Assets/Scripts/Paul/PlaneScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour
+{
+
+    private GameObject actualPlane;
+
+    [SerializeField]
+    private GameObject[] interElementTab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualPlane = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDesactive()
+    {
+        this.gameObject.SetActive(false);
+        for (int i = 0; i < interElementTab.Length; i++)
+            interElementTab[i].SetActive(false);
+    }
+
+    public void OnActive()
+    {
+        this.gameObject.SetActive(true);
+        for (int i = 0; i < interElementTab.Length; i++)
+            interElementTab[i].SetActive(true);
+    }
+}
