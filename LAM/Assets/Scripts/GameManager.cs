0d512ccdd3695d579c04a66b2f5d01d23195363d
Theo Driutti <theo.driutti57@gmail.com>
2019-10-03 11:30:02 +0200

    Inventaire

--- LAM/Assets/Scripts/GameManager.cs
+++ LAM/Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
+
     [SerializeField]
     private PlayerManager player;
     [SerializeField]
@@ -13,17 +15,35 @@ public class GameManager : MonoBehaviour
     private GameObject firstChamber;
     [SerializeField]
     private GameObject firstMask;
+    [SerializeField]
+    private GameObject secondMask;
+    [SerializeField]
+    private GameObject maskCollection;
 
     private string clickedBtnName;
     private Vector3 currentMaskPosition;
 
     private Vector3 positionOnRightDoor = new Vector3(6f, 2.5f, 0);
     private Vector3 positionOnLeftDoor = new Vector3(-6f, 2.5f, 0);
+    Component[] components;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        components = maskCollection.GetComponentsInChildren(typeof(Image), true);
+
+        for (int i = 0; i < components.Length; i++)
+        {
+            GameObject newMask = Instantiate(components[i].gameObject, new Vector2(80f * i + 50f, 700f), Quaternion.identity);
+            newMask.transform.localScale = 0.9f * newMask.transform.localScale;
+            newMask.SetActive(false);
+            Destroy(newMask.GetComponent<Button>()); // enlève le clic sur le masque
+            foreach (Transform child in newMask.transform) // enlève le texte "Button"
+            {
+                Destroy(child.gameObject);
+            }
+            newMask.transform.SetParent(maskCollection.transform.parent.transform); // met le masque dans le canvas
+        }
     }
 
     // Update is called once per frame
@@ -37,7 +57,16 @@ public class GameManager : MonoBehaviour
         if (player.transform.position == currentMaskPosition)
         {
             //placer le mask dans l'inventaire
-            firstMask.SetActive(false); //pour l'instant on le désactive juste
+            EventSystem.current.currentSelectedGameObject.GetComponent<Mask>().SetIsFound(true);
+            EventSystem.current.currentSelectedGameObject.SetActive(false);
+            foreach (Component comp in maskCollection.transform.parent.GetComponentsInChildren(typeof(Image), true))
+            {
+                if (comp.gameObject.name == EventSystem.current.currentSelectedGameObject.name + "(Clone)")
+                {
+                    comp.gameObject.SetActive(true);
+                }
+            }
+            currentMaskPosition = new Vector3();
         }
     }
 
@@ -57,17 +86,19 @@ public class GameManager : MonoBehaviour
     {
         player.targetPosition = player.PLAYER_BASE_POS;
 
-        if(hallway.activeSelf)
+        if (hallway.activeSelf)
         {
             hallway.SetActive(false);
             firstChamber.SetActive(true);
             firstMask.SetActive(true);
+            secondMask.SetActive(false);
         }
         else
         {
             hallway.SetActive(true);
             firstChamber.SetActive(false);
             firstMask.SetActive(false);
+            secondMask.SetActive(true);
         }
     }
 
---
+++ LAM/Assets/Scripts/Theo/InventoryManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    public const int nbrItemMax = 10;
+    public GameObject[] itemList = new GameObject[nbrItemMax];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddItem(GameObject item)
+    {
+        Debug.Log("l'item " + item + " est ajouté à l'inventaire");
+        itemList[0] = item;
+    }
+
+    public bool IsInInventory(GameObject item)
+    { // check si l'item donné en paramètre est déjà dans l'inventaire ou non
+        int i = 0;
+        bool found = false;
+        while (!found && i < this.itemList.Length)
+        {
+            found = GameObject.ReferenceEquals(item, this.itemList[i]);
+            i++;
+        }
+        return found;
+    }
+}
