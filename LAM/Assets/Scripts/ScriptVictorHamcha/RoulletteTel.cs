b11ac0068dced416fc90a98f5471af19d3544979
VictorHamcha <victor.hamcha@edu.devinci.fr>
2019-11-18 01:54:10 +0100

    tel 99pourcent fini

--- LAM/Assets/Scripts/ScriptVictorHamcha/RoulletteTel.cs
+++ LAM/Assets/Scripts/ScriptVictorHamcha/RoulletteTel.cs
@@ -8,29 +8,82 @@ public class RoulletteTel : MonoBehaviour
     private bool selected; // boolen servant a savoir si l'aiguille est séléctioné
     public float offset;//offset
     public float rotZ;
+    public float roatmax;
+    public float roatmin;
     public GameObject roulette;
-  
-    
+    public int numéro;
+    private TelephoneManager telephonemanager;
+    public GameObject ancre;
+    public float distance=10;
 
     private void Start()
     {
-       
+        telephonemanager = roulette.GetComponent<TelephoneManager>();
     }
 
     void Update()
     {
         
+        
 
         if (selected == true)// si l'aiguille est séléctioné
         {
             Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - roulette.transform.position; // *position de la souris
-            rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;// trouve l'angle de rotation que doit prendre l'aiguille par rapport à la souris 
-            roulette.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);// rotation de l'aiguille égal a l'angle trouvé au dessus + offset
+            rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg+360;// trouve l'angle de rotation que doit prendre l'aiguille par rapport à la souris 
+            distance = Vector2.Distance(transform.position, ancre.transform.position);
+           
+        if(roatmin < rotZ && rotZ < roatmax)
+             roulette.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);// rotation de l'aiguille égal a l'angle trouvé au dessus + offset
+
+            
+
         }
+        
+            
+               
+            
+           
+            
+
+        
 
         if (Input.GetKeyUp(KeyCode.Mouse0))// si je lache le clic gauche de la souris la pièce est déléctioné
         {
+
+            //if (rotZ<roatmax+0.3f)
+            //{
+            //    rotZ += 0.1f;
+            //    roulette.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);// rotation de l'aiguille égal a l'angle trouvé au dessus + offset
+            //}
+            //else if (rotZ > roatmax-0.3f)
+            //{
+            //    rotZ -= 0.1f;
+            //    roulette.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);// rotation de l'aiguille égal a l'angle trouvé au dessus + offset
+            //}
+            //else
+            //{
+            //    roulette.transform.rotation = Quaternion.Euler(0f, 0f, roatmax + offset);// rotation de l'aiguille égal a l'angle trouvé au dessus + offset
+            //}
+            if (selected)
+            {
+                roulette.transform.rotation = Quaternion.Euler(0f, 0f, roatmax + offset);
+                if (telephonemanager.numtel.Count <= 5&& distance<=0.1)
+                {
+                    telephonemanager.numtel.Add(numéro);
+                    
+                }
+
+                if (telephonemanager.end == false && telephonemanager.goodnumber == true&& distance <= 0.1)
+                    telephonemanager.end = true;
+
+            }
+
+            Debug.Log(telephonemanager.numtel.Count);
+           
+           
             selected = false;
+            
+
         }
 
       
---
+++ LAM/Assets/Scripts/ScriptVictorHamcha/TelephoneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelephoneManager : MonoBehaviour
+{
+    public List<int> numtel = new List<int>();
+    public bool end;
+    public bool goodnumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numtel.Count == 6 && numtel[0] == 1 && numtel[1] == 1 && numtel[2] == 1 && numtel[3] == 1 && numtel[4] == 1 && numtel[5] == 1)
+        {
+            goodnumber = true;
+            
+        }
+        if (end&&goodnumber)
+        {
+            Debug.Log("youwin");
+        }
+    }
+}
