5d2b40a70b3d7b36f98f3a992452058ee43a46c5
Paul Thibaul <Paulthb1@gmail.com>
2019-11-20 18:36:29 +0100

    finished light on scene and add LightScript

---
+++ LAM/Assets/Scripts/Paul/LightScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    public float minIntensity = 0f;
+    public float maxIntensity = 0f;
+
+    private Light currentlight;
+
+    float random;
+
+    void Start()
+    {
+        currentlight = GetComponent<Light>();
+        random = Random.Range(0.0f, 65535.0f);
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time);
+        currentlight.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+        Debug.Log(currentlight.intensity);
+    }
+}
