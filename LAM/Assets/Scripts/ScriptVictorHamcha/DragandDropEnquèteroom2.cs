540380d845142d081066f37474dde3e98f74a1c5
victor <victor.hamcha@edu.devinci.fr>
2019-11-21 10:26:55 +0100

    enigme finie et commenté

--- LAM/Assets/Scripts/ScriptVictorHamcha/DragandDropEnquèteroom2.cs
+++ LAM/Assets/Scripts/ScriptVictorHamcha/DragandDropEnquèteroom2.cs
@@ -4,105 +4,104 @@ using UnityEngine;
 
 public class DragandDropEnquèteroom2 : MonoBehaviour
 {
-    private bool selected;
-    public List<Transform> slots = new List<Transform>();
-    public int numérodepiéce;
+    private bool selected;// es-ce que la pièce est séléctioné 
+    public List<Transform> slots = new List<Transform>();//slots ou drop les images 
     [SerializeField]
-    private float distance;
-    private SpriteRenderer _sprite;
-    private Vector2 initialposition;
-    private bool droped;
-    public List<GameObject> goodpiece = new List<GameObject>();
-    public List<bool> piececorrectpos = new List<bool>();
-    
-    // Start is called before the first frame update
+    private float distance;//distance entre la pièce séléctioné et les slots 
+    private SpriteRenderer _sprite;//spriterenderer de l'image 
+    private Vector2 initialposition;//valeur de la position initial de la pièce 
+    private bool droped;//vérifie si la pièce a été drop dans un des slots 
+    public List<GameObject> goodpiece = new List<GameObject>();//es-ce que la pièce fait partie des bonnes pièce 
+    public List<bool> piececorrectpos = new List<bool>();//vérirfie si la piece es a la bonne position 
+
+    // Start is called before the first frame update 
     void Start()
     {
-        _sprite = GetComponent<SpriteRenderer>();
-        StartCoroutine(waitforinitialisation());
+        _sprite = GetComponent<SpriteRenderer>();//récupère le sprite de la pièce 
+        StartCoroutine(waitforinitialisation());//laisse le temps au pièce de se placer  
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        if (selected)
+        if (selected)// si la pièce est séléctioné 
         {
-            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector2(cursorPos.x, cursorPos.y);//la pèce prend la position de la souris
-            _sprite.sortingOrder = 2;
-            
-        }
-
-        for (int i = 0; i <= 3; i++)
-        {
-
-            
-                float distgoodnumber = Vector2.Distance(goodpiece[i].transform.position, slots[i].position);
-            if (distgoodnumber == 0)
-            {
-
-                piececorrectpos[i] = true;
-
-            }
-            else
-            {
-                piececorrectpos[i] = false;
-            }
-
-           
-            
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);//prend la valeur de la positon de la souris par rapport a la caméra 
+            transform.position = new Vector2(cursorPos.x, cursorPos.y);//la pèce prend la position de la souris 
+            _sprite.sortingOrder = 2;//mets la pièce au premier pland  
 
         }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        for (int i = 0; i <= 2; i++)// vérifie pour toutes les pièce de la liste  
         {
-            
-            for (int i=0; i<=3;i++)
+
+
+            float distgoodnumber = Vector2.Distance(goodpiece[i].transform.position, slots[i].position);//distance entre une bonne pièce et et le slots de la même index 
+            if (distgoodnumber == 0)//si la pièce est dans le bon slot alors  
             {
-                
+
+                piececorrectpos[i] = true;// le booléen le vérifient devient true 
+
+            }
+            else// sinon 
+            {
+                piececorrectpos[i] = false;//le bouleen est faux  
+            }
+
+
+
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))//si on lève le click de la souris 
+        {
+
+            for (int i = 0; i <= 2; i++)//vérifie pour chacun des slors 
+            {
+
                 if (selected)
                 {
-                    distance = Vector2.Distance(transform.position, slots[i].position);
-                    if (distance <= 1)
+                    distance = Vector2.Distance(transform.position, slots[i].position);// la distance entre la pièce que l'on a dans la main et les slots 
+                    if (distance <= 1)//si la pièce est prets de l'un des slots alors 
                     {
-                        transform.position = slots[i].position;
-                        droped = true;
-                       
+                        transform.position = slots[i].position;//la pièce prend la position du slot en question 
+                        droped = true;//dropped devient true 
+
                     }
-                   
+
 
                 }
-                
-              
+
+
             }
-            if (!droped && selected)
+            if (!droped && selected)// si la pièce séléctioné n'a pas été drop dans un slot alors... 
             {
-                transform.position = initialposition;
+                transform.position = initialposition;//la pièce reprend sa positon initial 
             }
-            _sprite.sortingOrder = 0;
-            selected = false;
-            droped = false;
+            _sprite.sortingOrder = 0;//la pièce revient au plan de base 
+            selected = false;// pièce desélctioné  
+            droped = false;//droped devient faux  
         }
 
-        if (piececorrectpos[0]&& piececorrectpos[1] && piececorrectpos[2] && piececorrectpos[3])
+        if (piececorrectpos[0] && piececorrectpos[1] && piececorrectpos[2])//si toutes les pièces sont au bonnes endroits 
         {
             Debug.Log("youwin");
         }
-        
+
     }
 
-    IEnumerator waitforinitialisation ()
+    IEnumerator waitforinitialisation()// coroutine servent a attendre que les pièce se soit placer a leur bonnes places  
     {
-        yield return new WaitForSeconds(0.1f);
-        initialposition = transform.position;
-        
+        yield return new WaitForSeconds(0.1f);//attend que les pièce soit a leur bonne place 
+        initialposition = transform.position;//initial positon prend la valeur de la position initial des pièce  
+
     }
 
-    private void OnMouseOver()
+    private void OnMouseOver()//si on a la souris sur une pièce 
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))// si j'appuye sur click gauche alors... 
         {
-            selected = true;
+            selected = true;//la pièce est séléctionné 
         }
     }
 }
--- LAM/Assets/Scripts/ScriptVictorHamcha/EnqèteManager.cs
+++ LAM/Assets/Scripts/ScriptVictorHamcha/EnqèteManager.cs
@@ -5,33 +5,33 @@ using System.Linq;
 
 public class EnqèteManager : MonoBehaviour
 {
-    public List<GameObject> imagetodrag = new List<GameObject>();
-    private List<GameObject> imagetodragrandomized = new List<GameObject>();
-    public float x;
-    public float y;
-    private int nbrdecare = 0;
-    // Start is called before the first frame update
+    public List<GameObject> imagetodrag = new List<GameObject>();//les images qu'ils faut drag 
+    private List<GameObject> imagetodragrandomized = new List<GameObject>();//les images qu'il faut drag mélangé 
+    public float x;//positon en X des pièce 
+    public float y;//position en Y de la pièce 
+    private int nbrdecare = 0;// nbr de pièce par collone 
+    // Start is called before the first frame update 
     void Start()
     {
-       imagetodragrandomized  = imagetodrag.OrderBy(g => Random.value).ToList();//nouvelle liste comprenant les pièce mais dans un ordre aléatoire
-        for (int i=0; i<=8;i++)
+        imagetodragrandomized = imagetodrag.OrderBy(g => Random.value).ToList();//nouvelle liste comprenant les pièce mais dans un ordre aléatoire 
+        for (int i = 0; i <= 7; i++)//pour toutes les pièces 
         {
-            
-            imagetodragrandomized[i].transform.position = new Vector2(x, y);
-            nbrdecare += 1;
-            x += 2.8f;
-            if (nbrdecare == 5)
+
+            imagetodragrandomized[i].transform.position = new Vector2(x, y);//la positon de la pièce est eagal a X et Y 
+            nbrdecare += 1;//retiens qu'il y a une pièce de plus sur la colomne 
+            x += 2.8f;//la prochaine pièce est à 2.8f de la dernière place posée 
+            if (nbrdecare == 4)//si y a deja 4 pièce sur la colomne 
             {
-                y -= 2.4f;
-                x = -6.41f;
+                y -= 2.4f;// la pochaine pièce se place à 2.4f en dessous  
+                x = -6.41f;// sur la première ligne 
             }
 
         }
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        
+
     }
 }
