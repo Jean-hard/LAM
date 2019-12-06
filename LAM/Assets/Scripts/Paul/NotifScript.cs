386b989905f31367bc22c0b0f416cb4330006e68
Paul Thibaul <Paulthb1@gmail.com>
2019-12-06 14:47:17 +0100

    delete old system + add new notif sytem

--- LAM/Assets/Scripts/Paul/NotifScript.cs
+++ LAM/Assets/Scripts/Paul/NotifScript.cs
@@ -1,48 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NotificationSamples;
 using System;
 
 public class NotifScript : MonoBehaviour
 {
-    [SerializeField]
-    private GameNotificationsManager notificationsManager;
+    //[SerializeField]
+    //private GameNotificationsManager notificationsManager;
 
-    private void StartNotification()
-    {
-        GameNotificationChannel channel = new GameNotificationChannel("Fausse piste",
-            "putain c'est fausse piste ou l'ile au maléfice ?!",
-            "dites le moi plz");
-        notificationsManager.Initialize(channel);
-    }
+    //private void StartNotification()
+    //{
+    //    GameNotificationChannel channel = new GameNotificationChannel("Fausse piste",
+    //        "putain c'est fausse piste ou l'ile au maléfice ?!",
+    //        "dites le moi plz");
+    //    notificationsManager.Initialize(channel);
+    //}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartNotification();
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    StartNotification();
+    //}
 
-    public void ShowNotificationAfterDelay(int sec)
-    {
-        ShowNotificationAfterDelay("Fausse piste", "putain c'est fausse piste ou l'ile au maléfice ?!",
-            DateTime.Now.AddSeconds(sec));
-        Debug.Log("notification envoyé !!");
-    }
+    //public void ShowNotificationAfterDelay(int sec)
+    //{
+    //    ShowNotificationAfterDelay("Fausse piste", "putain c'est fausse piste ou l'ile au maléfice ?!",
+    //        DateTime.Now.AddSeconds(sec));
+    //    Debug.Log("notification envoyé !!");
+    //}
 
-    private void ShowNotificationAfterDelay(string title, string body, DateTime time)
-    {
-        IGameNotification createNotification = notificationsManager.CreateNotification();
-        if(createNotification != null)
-        {
-            createNotification.Title = title;
-            createNotification.Body = body;
-            createNotification.DeliveryTime = time;
+    //private void ShowNotificationAfterDelay(string title, string body, DateTime time)
+    //{
+    //    IGameNotification createNotification = notificationsManager.CreateNotification();
+    //    if(createNotification != null)
+    //    {
+    //        createNotification.Title = title;
+    //        createNotification.Body = body;
+    //        createNotification.DeliveryTime = time;
 
-            //je le fais à contre coeur
-            var notificationToDisplay = notificationsManager.ScheduleNotification(createNotification);
-        }
-    }
+    //        //je le fais à contre coeur
+    //        var notificationToDisplay = notificationsManager.ScheduleNotification(createNotification);
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
