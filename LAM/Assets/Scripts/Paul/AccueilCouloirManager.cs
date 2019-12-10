2cf01cb011141439c3f84adfcdead2e064aad5e5
Paul Thibaul <Paulthb1@gmail.com>
2019-12-10 11:50:37 +0100

    add accueilCouloirManager + updtae quality settings

---
+++ LAM/Assets/Scripts/Paul/AccueilCouloirManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccueilCouloirManager : MonoBehaviour
+{
+    [SerializeField]
+    private Dialogue aubergisteTalkDialogue;
+
+    public static bool aubergisteTalkDone;
+    public void CheckAubergisteTalkDone(DoorScript stairDoor)
+    {
+        Debug.Log("aubergiste talk : " + aubergisteTalkDone);
+        if (aubergisteTalkDone)
+        {
+            GameManager.Instance.MoveToDoor(stairDoor);
+        }
+        else
+        {
+            GameManager.Instance.InitDialogue(aubergisteTalkDialogue);
+        }
+    }
+}
