ce5ec8c29c384c36fb0bf0da53608ceb2059d16a
Paul Thibaul <Paulthb1@gmail.com>
2020-01-26 15:14:37 +0100

    add Intro video ready
    quand l'intro sera faite, il faudra la mettre dans le introcinematique puis la filer à ecranTitre

--- LAM/Assets/Scripts/Jean/TitreManager.cs
+++ LAM/Assets/Scripts/Jean/TitreManager.cs
@@ -2,23 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class TitreManager : MonoBehaviour
 {
     public FadeScript fadeScreen;
     public FadeScript casqueScreen;
+    public VideoPlayer introVideo;
+
+    private void Start()
+    {
+        if (introVideo)
+            introVideo.loopPointReached += EndofIntro;
+    }
 
     public void LoadGame()
     {
-        fadeScreen.FadeIn();
+        //fadeScreen.FadeIn();
+        casqueScreen.FadeIn();
         StartCoroutine(WaitForLoadGame());
     }
 
     public IEnumerator WaitForLoadGame()
     {
         yield return new WaitForSeconds(2f);
-        casqueScreen.FadeIn();
-        yield return new WaitForSeconds(4f);
+        //fadeScreen.FadeOut();
+    
+        yield return new WaitForSeconds(2f);
+        casqueScreen.FadeOut();
+
+        if (introVideo)
+            introVideo.Play();
+        else
+        {
+            fadeScreen.FadeIn();
+            Debug.Log("changement sans video");
+            yield return new WaitForSeconds(4f);
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void EndofIntro(UnityEngine.Video.VideoPlayer vp)
+    {
+        Debug.Log("changement en fin de video");
         SceneManager.LoadScene(1);
     }
 }
